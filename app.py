e7ac8c79a1ab5b7754a99c8974831ff506f8ad88
Loony213 <kevinhdhd@hotmail.com>
2025-06-30 22:42:48 -0500

    feat: :sparkles: new format

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-from Login.login import login_bp
-from Register.register import register_bp
+from routes.login_routes import login_bp
+from routes.register_routes import register_bp
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(login_bp, url_prefix='/api/auth')
-app.register_blueprint(register_bp, url_prefix='/api/auth')
+app.register_blueprint(login_bp)
+app.register_blueprint(register_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(host='0.0.0.0', port=5000)
