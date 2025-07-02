587aee0a09659e4c788badafea021c41fe1a7304
Loony213 <150311939+Loony213@users.noreply.github.com>
2025-07-02 12:53:38 -0500

    Change code

--- Register/app.py
+++ Register/app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.login_routes import login_bp
 from routes.register_routes import register_bp
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(login_bp)
 app.register_blueprint(register_bp)
 
 if __name__ == '__main__':
