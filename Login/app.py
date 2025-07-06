0f8a715625d7bc0b9d4296cafdaa804271c3b29d
Loony213 <150311939+Loony213@users.noreply.github.com>
2025-07-05 21:18:48 -0500

    test

--- Login/app.py
+++ Login/app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from routes.login_routes import login_bp
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(login_bp)
+# Ruta Health Check
+@app.route('/healthcheck', methods=['GET'])
+def healthcheck():
+    return jsonify({"status": "healthy"}), 200
 
+# Registrar Blueprint de login
+app.register_blueprint(login_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
